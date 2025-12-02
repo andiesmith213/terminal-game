8db3410edb73dc317528784741e5441ed6ecdbf2
andiesmith213 <andie.smith213@gmail.com>
2025-12-01 21:25:35 -0700

    Initial check in of main python file

---
+++ main.py
@@ -0,0 +1,12 @@
+#Pokemon/MTG card game
+#Each player selects 5 different cards from a list
+#There will be Attack cards, Spell cards and Defense cards
+#The game will go for 3 rounds and each player will get a new, randomly selected card at the end of their turn
+#After 3 rounds, the player with the most health wins
+
+#Create a class for the player, a class for the players deck and classes for each card type
+#The player class will contain their HP, an attack attribute and a defense attribute
+#The card classes will contain methods with the modifiers it will place on the player class
+#Defense cards will take damage for the player
+#Attack cards will do damage to the other player
+#Spell cards will do 1 of 2 things: provide the player with HP or inflict damage to the other player (i.e. bypass any defenders)
